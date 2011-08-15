20faaf819635a776b442c8c469a5b4876e3b9748
josealvarezmuguerza <muguerza@intalio.com>
2011-08-15 10:37:46 -0300

    TURMERIC-1470 - adding get method

--- utils/turmeric-utils-cassandra/src/main/java/org/ebayopensource/turmeric/utils/cassandra/HectorManager.java
+++ utils/turmeric-utils-cassandra/src/main/java/org/ebayopensource/turmeric/utils/cassandra/HectorManager.java
@@ -1,116 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2010 eBay Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *******************************************************************************/
 package org.ebayopensource.turmeric.utils.cassandra;
 
 import me.prettyprint.cassandra.serializers.StringSerializer;
-import me.prettyprint.cassandra.service.CassandraHostConfigurator;
 import me.prettyprint.hector.api.Cluster;
 import me.prettyprint.hector.api.Keyspace;
+import me.prettyprint.hector.api.beans.ColumnSlice;
 import me.prettyprint.hector.api.factory.HFactory;
 import me.prettyprint.hector.api.mutation.Mutator;
-import me.prettyprint.hector.api.query.ColumnQuery;
-import me.prettyprint.hector.api.query.Query;
-
+import me.prettyprint.hector.api.query.QueryResult;
+import me.prettyprint.hector.api.query.SliceQuery;
 
 public class HectorManager {
 
 	private static Keyspace keyspace;
 	private final static StringSerializer serializer = StringSerializer.get();
-	
+
 	private static Mutator mutator;
-	
-	public static Cluster getOrCreateCluster(){
-		//retreive form properties
-		//clusterName, hostIp
-		return HFactory.getOrCreateCluster("TurmericCluster", "192.168.2.101:9160");
+
+	public static Cluster getOrCreateCluster() {
+		// retreive form properties
+		// clusterName, hostIp
+		return HFactory.getOrCreateCluster("TurmericCluster",
+				"192.168.2.101:9160");
 	}
-	
-	public static Keyspace getKeyspace(final String space){
-		//retreive form properties
-		//Keyspace
-		if(keyspace == null){
+
+	public static Keyspace getKeyspace(final String space) {
+		// Keyspace name retrieve form properties
+		if (keyspace == null) {
 			keyspace = HFactory.createKeyspace(space, getOrCreateCluster());
 			mutator = HFactory.createMutator(keyspace, serializer);
 		}
 		return keyspace;
 	}
-	
-	
-	
-	public static void insert(final String key, final String param, final String name, final String value){
+
+	public static void insert(final String columnFamily, final String key,
+			final String name, final String value) {
 		try {
-			mutator.addInsertion(key, param,
+
+			mutator.insert(key, columnFamily,
 					HFactory.createStringColumn(name, value));
-			mutator.execute();
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
-//	public static String get(final String key, final String column, final String columnFamily  ){
-//		
-//		
-//		Query que =  new Query();
-//		que.
-//		
-//		ColumnQuery q = HFactory.createColumnQuery(keyspace, serializer, serializer, serializer);
-//		// set key, name, cf and execute
-//		Result r = q.setKey("key1").
-//		        setName(column).
-//		        setColumnFamily(columnFamily).
-//		        execute();
-//		// read value from the result
-//		HColumn c = r.get();
-//		String value =  c.getValue();
-//
-//		
-//		
-//	}
-//	
-	
-	
-	
-//	
-//	private static final Mutator<String> rlMutator;
-//	private static final Keyspace rlKeyspace ;
-//	public static Cluster getOrCreateCluster(){
-//		//retreive form properties
-//		//clusterName, hostIp
-//		return HFactory.getOrCreateCluster("TurmericCluster", "localhost:9160");
-//	}
-//	
-//	public static Keyspace getRLKeyspace(){
-//		//retreive form properties
-//		//RLKeyspace
-//		if(rlKeyspace == null){
-//			rlKeyspace = HFactory.createKeyspace("RLKeyspace", getOrCreateCluster());
-//		}
-//		return rlKeyspace;
-//	}
-//	
-//	public static Mutator<String> getRLMutator(){
-//		if(rlMutator == null){
-//			rlMutator = HFactory.createMutator(rlKeyspace, StringSerializer.get());
-//		}
-//		return rlMutator;
-//	}
-//	 
-//	public static void insert()
-//	 Keyspace keyspaceOperator = HFactory.createKeyspace("Keyspace1", cluster);
-//     try {
-//         Mutator<String> mutator = HFactory.createMutator(keyspaceOperator, StringSerializer.get());
-//         mutator.insert("jsmith", "Standard1", HFactory.createStringColumn("first", "John"));
-//         
-//         ColumnQuery<String, String, String> columnQuery = HFactory.createStringColumnQuery(keyspaceOperator);
-//         columnQuery.setColumnFamily("Standard1").setKey("jsmith").setName("first");
-//         QueryResult<HColumn<String, String>> result = columnQuery.execute();
-//         
-//         System.out.println("Read HColumn from cassandra: " + result.get());
-//         System.out.println("Verify on CLI with: get Keyspace1.Standard1['jsmith'] ");
-//         
-//     } catch (HectorException e) {
-//         e.printStackTrace();
-//     }
-//     cluster.getConnectionManager().shutdown
-//     
+
+	public static String get(final String columnFamily, final String column,
+			final String key) {
+
+		SliceQuery<String, String, String> createSliceQuery = HFactory
+				.createSliceQuery(keyspace, serializer, serializer, serializer);
+		createSliceQuery.setColumnFamily(columnFamily);
+		createSliceQuery.setColumnNames(column);
+		createSliceQuery.setKey(key);
+		QueryResult<ColumnSlice<String, String>> execute2 = createSliceQuery
+				.execute();
+		execute2.get().getColumnByName(column).getValue();
+
+		return execute2.toString();
+	}
+
 }
